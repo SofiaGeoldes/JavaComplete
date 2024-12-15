2dbe85c1a8bb1ae283e04336f8356311b0706447
Sofia Geoldes <sofiageoldes92@gmail.com>
2024-12-15 12:42:09 +0200

    Tema TreeSet

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        //Create TreeSets sorted by Name & by Age
+        //Create TreeSets sorted by Name and add Persons to be sorted
         TreeSet<Person> sortedNameSet = new TreeSet<>(new CompareByName());
 
         sortedNameSet.add(new Person ("Remus", 28));
@@ -19,7 +19,8 @@ public class App
         sortedNameSet.add(new Person ("Darius", 45));
         sortedNameSet.add(new Person ("Pavel", 60));
 
-       TreeSet<Person> sortedAgeSet = new TreeSet<>(new CompareByAge());
+        //Create TreeSet sorted by Age and add Persons to be sorted
+        TreeSet<Person> sortedAgeSet = new TreeSet<>(new CompareByAge());
 
         sortedAgeSet.add(new Person ("Remus", 28));
         sortedAgeSet.add(new Person ("Raluca", 22));
@@ -28,62 +29,73 @@ public class App
         sortedAgeSet.add(new Person ("Pavel", 60));
 
         //Using the for loop to iterate and print the treeSet sorted by Name
+        System.out.println("\t");
+        System.out.println("Sort the list of Persons by Name");
+        System.out.println("\t");
         System.out.println("Name\tAge");
         for(Person person : sortedNameSet) {
             System.out.println(person.getName() + "\t" + person.getAge());
         }
 
        //Using a for loop to iterate and print the treeSet sorted by Age
-        System.out.println("Age\tName");
+        System.out.println("\t");
+        System.out.println("Sort the list of Persons by Age");
+        System.out.println("\t");
+        System.out.println("Name\tAge");
+        System.out.println("\t");
         for(Person person : sortedAgeSet){
-            System.out.println(person.getAge() + "\t" + person.getName());
+            System.out.println(person.getName() + "\t" + person.getAge());
         }
 
-        //Creating countries and address
+        //Creating countries and addresses
         Country spain = new Country("Spain");
-        Address address1 = new Address("Barcelona", spain);
+        Address barcelona = new Address("Barcelona", spain);
         Country italy = new Country("Italy");
-        Address address2 = new Address("Florence", italy);
-        Country usa = new Country("usa");
-        Address address3 = new Address("New York", usa);
+        Address florence = new Address("Florence", italy);
+        Country usa = new Country("USA");
+        Address newYork = new Address("New York", usa);
         Country greece = new Country("Greece");
-        Address address4 = new Address("Creta", greece);
+        Address atena = new Address("Atena", greece);
 
-        List<Address> addresses = new ArrayList<Address>();
-        addresses.add(address1);
-        addresses.add(address2);
-        addresses.add(address3);
-        addresses.add(address4);
+        //Creating Hobbies
+        Hobby dancing = new Hobby("Dancing", 5,Arrays.asList(barcelona, atena));
+        Hobby cooking = new Hobby("Cooking", 2,Arrays.asList(florence));
+        Hobby fishing = new Hobby("Fishing", 4,Arrays.asList(atena, newYork, barcelona));
+        Hobby golfing = new Hobby("Golf", 7, Collections.singletonList(atena));
 
-        Hobby hobby1 = new Hobby("Dancing", 5, addresses);
-        Hobby hobby2= new Hobby("Cooking", 2,addresses);
-        Hobby hobby3 = new Hobby("Fishing", 4,addresses);
-        Hobby hobby4 = new Hobby("Golf", 7,addresses);
+        //Adding them to the list of Hobbies
+        ArrayList<Hobby> hobbies = new ArrayList<Hobby>();
+        hobbies.add(dancing);
+        hobbies.add(cooking);
+        hobbies.add(fishing);
+        hobbies.add(golfing);
 
-        List<Hobby> hobbies = new ArrayList<Hobby>();
-        hobbies.add(hobby1);
-        hobbies.add(hobby2);
-        hobbies.add(hobby3);
-        hobbies.add(hobby4);
-
-        Person person1 = new Person("Anemona", 18);
+        //Linking each Person to the hobbies list using a HashMap
         HashMap<Person, List<Hobby>> personHobbies = new HashMap<>();
-        personHobbies.put(person1, hobbies);
+        Person person1 = new Person("Anemona", 18);
+        personHobbies.put(person1, Arrays.asList(dancing, cooking));
 
         Person person2 = new Person("Darius", 45);
-        HashMap<Person, List<Hobby>> personHobbies1 = new HashMap<>();
-        personHobbies.put(person2, hobbies);
+        personHobbies.put(person2, Arrays.asList(fishing, golfing, dancing));
 
         Person person3 = new Person("Pavel", 60);
-        HashMap<Person, List<Hobby>> personHobbies2 =  new HashMap<>();
-        personHobbies.put(person3, hobbies);
+        personHobbies.put(person3, Arrays.asList(fishing, golfing));
 
+        Person person4 = new Person("Remus", 28);
+        personHobbies.put(person4, hobbies);
+
+        Person person5 = new Person("Raluca", 22);
+        personHobbies.put(person5, Arrays.asList(fishing, cooking, golfing));
+
+        //Get the list of hobbies and their countries for every person
         for(Map.Entry<Person, List<Hobby>> entry : personHobbies.entrySet()) {
-            Person p = entry.getKey();
-            System.out.println(p.getName() + " 's hobbies: ");
+            Person person = entry.getKey();
+            System.out.println("\t");
+            System.out.println(person.getName() + " 's hobbies: ");
             for(Hobby h : entry.getValue()) {
-                System.out.println("- " + h.hobbyName + " practiced " + h.frequency + " times a week " + h.addresses.get(1).country.name);
+                System.out.println("- " + h.hobbyName + " practiced " + h.frequency + " times a week in " + h.addresses.get(0).address + ", " + h.addresses.get(0).country.name);
+                }
             }
         }
     }
-}
+
